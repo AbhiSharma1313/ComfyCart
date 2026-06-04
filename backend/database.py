c0d65714d9169592fb13e766237349c865b64ce7
Abhishek Sharma <abhisharma132804@gmail.com>
2026-06-04 17:55:12 +0530

    Clarify DATABASE_URL password input in database.py
    Updated DATABASE_URL comment to clarify password input.

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Your_Password@localhost/comfycart"
+DATABASE_URL = "mysql+pymysql://root:Your_Password@localhost/comfycart"  #Add your root password here.
 
 engine = create_engine(DATABASE_URL)
 
